ce9a7236d5c2bd4a7ee44898bc9801f20d2f0b0e
Kadir Akipek <akipekkadir23@gmail.com>
2025-06-16 09:30:40 +0300

    Add project files.

---
+++ RabbitMQ.Consumer/Program.cs
@@ -0,0 +1,33 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+//Bağlantı Oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://nhmssnpr:lCdcfPi66bVVc8bUi0LH76YGN8LeIlrO@shark.rmq.cloudamqp.com/nhmssnpr");
+
+
+//Bağlantı aktifleştirme ve kanal açma
+using IConnection connection = factory.CreateConnection();
+using IModel channel = connection.CreateModel();
+
+
+//Queue oluşturma
+channel.QueueDeclare();
+//Consumer'dada kuyruk publisher'daki ile birebir aynı yapılandırmayla tanımlanmalıdır.
+channel.QueueDeclare(queue: "example-queue", exclusive: false);
+
+
+//Queue'den mesaj okuma
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(queue: "example-queue", false, consumer);
+
+consumer.Received += (sender, e) =>
+{
+	//kuyruğa gelen mesajın işlendiği yer
+	//e.Body : Kuyruktaki mesajın verisini bütünsel olarak getirecektir
+	//e.Body.Span veya e.Body.ToArray() : kuyruktaki mesajın byte verisini getirecektir
+	Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+};
+
+Console.Read();
---
+++ RabbitMQ.Publisher/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+//Bağlantı oluşturma
+ConnectionFactory factory = new();
+factory.Uri = new("amqps://nhmssnpr:lCdcfPi66bVVc8bUi0LH76YGN8LeIlrO@shark.rmq.cloudamqp.com/nhmssnpr");
+
+
+//Bağlantı aktifleştirme ve kanal açma
+using IConnection connection =  factory.CreateConnection();
+using IModel channel =  connection.CreateModel();
+
+
+//Queue oluşturma
+channel.QueueDeclare(queue: "example-queue", exclusive:false);
+
+//Queue'ya mesaj gönderme
+
+//RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir. Bu yüzden mesajları byte türüne dönüştürmemiz gerekir
+
+for (int i = 0; i < 10; i++)
+{
+	await Task.Delay(200);
+	byte[] message = Encoding.UTF8.GetBytes("Merhaba " + i);
+	channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+}
+
+Console.Read();
