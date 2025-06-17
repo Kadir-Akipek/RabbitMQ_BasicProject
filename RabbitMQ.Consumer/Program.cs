16379824acfd106dfac71187690816840ec0175d
Kadir Akipek <akipekkadir23@gmail.com>
2025-06-17 16:14:37 +0300

    Queues are improved with Message Acknowledgement, Message Durability, Fair Dispatch

--- RabbitMQ.Consumer/Program.cs
+++ RabbitMQ.Consumer/Program.cs
@@ -15,12 +15,14 @@ using IModel channel = connection.CreateModel();
 //Queue oluşturma
 channel.QueueDeclare();
 //Consumer'dada kuyruk publisher'daki ile birebir aynı yapılandırmayla tanımlanmalıdır.
-channel.QueueDeclare(queue: "example-queue", exclusive: false);
+channel.QueueDeclare(queue: "example-queue", exclusive: false, durable: true);
 
 
 //Queue'den mesaj okuma
 EventingBasicConsumer consumer = new(channel);
-channel.BasicConsume(queue: "example-queue", false, consumer);
+channel.BasicConsume(queue: "example-queue", autoAck: false, consumer);
+channel.BasicQos(0, 1, false);
+
 
 consumer.Received += (sender, e) =>
 {
@@ -28,6 +30,7 @@ consumer.Received += (sender, e) =>
 	//e.Body : Kuyruktaki mesajın verisini bütünsel olarak getirecektir
 	//e.Body.Span veya e.Body.ToArray() : kuyruktaki mesajın byte verisini getirecektir
 	Console.WriteLine(Encoding.UTF8.GetString(e.Body.Span));
+	channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
 };
 
 Console.Read();
--- RabbitMQ.Publisher/Program.cs
+++ RabbitMQ.Publisher/Program.cs
@@ -12,17 +12,20 @@ using IModel channel =  connection.CreateModel();
 
 
 //Queue oluşturma
-channel.QueueDeclare(queue: "example-queue", exclusive:false);
+channel.QueueDeclare(queue: "example-queue", exclusive:false, durable: true);
 
 //Queue'ya mesaj gönderme
 
 //RabbitMQ kuyruğa atacağı mesajları byte türünden kabul etmektedir. Bu yüzden mesajları byte türüne dönüştürmemiz gerekir
 
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
 for (int i = 0; i < 10; i++)
 {
 	await Task.Delay(200);
 	byte[] message = Encoding.UTF8.GetBytes("Merhaba " + i);
-	channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message);
+	channel.BasicPublish(exchange: "", routingKey: "example-queue", body: message, basicProperties:properties);
 }
 
 Console.Read();
