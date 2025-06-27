12e22917213d6da72507bb7d19122c38491f22ec
Kadir_Akipek <Kadir@DESKTOP-N079G7I>
2025-06-27 09:03:14 +0300

    other message patterns added

--- RabbitMQ.Message.Templates.Publisher/Program.cs
+++ RabbitMQ.Message.Templates.Publisher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 using RabbitMQ.Client.Exceptions;
 using System.Text;
 
@@ -27,28 +28,103 @@ using IModel channel = connection.CreateModel();
 #endregion
 
 #region pub/sub
-string exchangeName = "example-pub-sub-exchange";
+//string exchangeName = "example-pub-sub-exchange";
 
-channel.ExchangeDeclare(
-    exchange: exchangeName,
-    type: ExchangeType.Fanout
-    );
+//channel.ExchangeDeclare(
+//    exchange: exchangeName,
+//    type: ExchangeType.Fanout
+//    );
+
+//for (int i = 0; i < 10; i++)
+//{
+
+//    await Task.Delay(200);
+
+//    byte[] message = Encoding.UTF8.GetBytes("mergaba" + i);
+
+//    channel.BasicPublish(
+//        exchange: exchangeName,
+//        routingKey: string.Empty,
+//        body: message
+//        );
+//}
+
+#endregion 
+
+#region work-queue
+
+//string queueName = "example-work-queue";
+
+//channel.QueueDeclare(
+//    queue: queueName,
+//    durable: false,
+//    exclusive: false,
+//    autoDelete: false);
+
+//for (int i = 0; i < 10; i++)
+//{
+
+//    await Task.Delay(200);
+
+//    byte[] message = Encoding.UTF8.GetBytes("mergaba" + i);
+
+//    channel.BasicPublish(
+//        exchange: string.Empty,
+//        routingKey: queueName,
+//        body: message
+//        );
+//}
+
+#endregion
+
+#region request-response
+
+string requestQueueName = "example-request-response-queue";
+
+channel.QueueDeclare(
+    queue: requestQueueName,
+    durable: false,
+    exclusive: false,
+    autoDelete: false);
+
+string replyQueueName = channel.QueueDeclare().QueueName;
+
+string correlationId = Guid.NewGuid().ToString();
+
+//Request mesajını oluşturma e gönderme
+
+IBasicProperties properties = channel.CreateBasicProperties();
+properties.CorrelationId = correlationId;
+properties.ReplyTo = replyQueueName;
 
 for (int i = 0; i < 10; i++)
 {
-
-    await Task.Delay(200);
-
-    byte[] message = Encoding.UTF8.GetBytes("mergaba" + i);
-
+    byte[] message = Encoding.UTF8.GetBytes("merhaba" + i);
     channel.BasicPublish(
-        exchange: exchangeName,
-        routingKey: string.Empty,
-        body: message
-        );
+        exchange: string.Empty,
+        routingKey: requestQueueName,
+        body: message,
+        basicProperties: properties);
 }
 
+//Response Kuyruğu Dinleme
+
+EventingBasicConsumer consumer = new(channel);
+channel.BasicConsume(
+    queue: replyQueueName,
+    autoAck: true,
+    consumer: consumer);
+
+consumer.Received += (sender, e) =>
+{
+    if (e.BasicProperties.CorrelationId == correlationId)
+    {
+        Console.WriteLine($"response : {Encoding.UTF8.GetString(e.Body.Span)}");
+    }
+};
+
 #endregion
+
 
 
 Console.Read();
